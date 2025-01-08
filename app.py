df8d55bc23ca077c60236549474aeb82294dc00c
NkenfaB <67001325+NkenfaB@users.noreply.github.com>
2025-01-08 10:07:48 +0100

    deploying on azure

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, jsonify
+# zenify_backend/app.py
 
-app = Flask(__name__)
+import os
+from flask import Flask
+from config import Config
+
+# Import your controllers
+from controller.chat_controller import chat_controller
+from controller.speech_controller import speech_controller
 
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return jsonify({"message": "Hello, welcome to the Flask app!"})
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-@app.route('/goodbye', methods=['GET'])
-def goodbye():
-    return jsonify("GOODBYE LITTLE GIRL!!")
+    # Register Blueprints
+    app.register_blueprint(chat_controller, url_prefix='/api')
+    app.register_blueprint(speech_controller, url_prefix='/api')
+    
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Create the Flask app
+    flask_app = create_app()
+    # Run the server
+    flask_app.run(debug=True, host='0.0.0.0', port=5000)
